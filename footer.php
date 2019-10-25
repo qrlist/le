fab5194ccb7ac7b94acaa2d3fef35dc8a02723e2
qrlist <49570994+qrlist@users.noreply.github.com>
2019-10-25 14:55:57 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+</body>
+<footer >
+ <div class="ui inverted vertical footer segment">
+                <div class="ui container">
+                    <div class="ui stackable inverted divided equal height stackable grid">
+                        <div class="three wide column">
+                            <h4 class="ui inverted header">Como usar o Site</h4>
+                            <div class="ui inverted link list">
+                                <a href="../views/home.php#listaNormal" class="item">Lista sem o QrCode</a>
+                                <a href="../views/home.php#listaQr" class="item">Lista com o QrCode?</a>
+                                <h4 class="ui inverted header">Sobre Nós</h4>
+                                <a href="../views/home.php#quemsomos" class="item">Quem somos?</a>
+                                <a href="https://araquari.ifc.edu.br/" class="item">A Instituição</a>
+                            </div>
+                        </div>
+                        <div class="three wide column">
+                            <h4 class="ui inverted header">Atalhos</h4>
+                            <div class="ui inverted link list">
+                                <a href="./views/login.php" class="item">Login</a>
+                                <a href="./views/usuario_cadastro.php" class="item">Cadastro Usuario</a>
+                                <a href="./views/mercado_pagina.php" class="item">Mercado cadastrados</a>
+                            </div>
+                        </div>
+                        <div class="seven wide column">
+                            <h4 class="ui inverted header">Deseja fazer parte do QrList?</h4>
+                            <p>Se você é dono de mercado e deseja participar da comunidade QrList, cadastre seu mercado <a href="../views/mercado_cadastro.php">aqui</a>!</p>
+                            <p>Se seu mercado já possui cadastro clique <a href="../views/mercado_login.php">aqui</a>!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</footer>
+</html>
