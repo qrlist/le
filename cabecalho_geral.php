7c2d76c8d2005cbd2aeed1da692e05163406cadb
qrlist <49570994+qrlist@users.noreply.github.com>
2019-10-25 15:28:39 -0300

    Add files via upload

--- cabecalho_geral.php
+++ cabecalho_geral.php
@@ -1,40 +1,58 @@
 <?php
- require __DIR__.'/cabecalho.php';
+require __DIR__.'/cabecalho.php';
 ?>
 <?php if (!isset($_SESSION['cpf'])) :?>
-<nav>
-<div class="ui inverted menu">
-  <a class="active item" href="../views/home.php">
-    Home
-  </a>
-  <a class="item" href="../views/usuario_login.php">
-    Logar
-  </a>
-  <a class="item" href="../views/usuario_cadastro.php">
-    Cadastrar
-  </a>
-</div>
-</nav>
-<?php else: ?>
-<nav>
-<div class="ui inverted menu">
-    <a class="active item" href="../views/home.php">
-        Home
-    </a>
-    <a class="item" href='../controllers/usuario.php?acao=usuario_delete'>
-        Deletar conta
-    </a>
-    <a class="item" href="../views/confirmacao_delete.php">
-        Deletar conta teste
-    </a>
-    <a class="item" href='../views/usuario_editar.php'>
-        Editar Informações
-    </a>
-    <div class="right menu">
-        <a href="../controllers/usuario.php?acao=usuario_deslogar" class="ui item">
-            Deslogar
+    <nav>
+        <div class="ui inverted menu">
+          <a class="active item" href="../views/home.php">
+            Home
+        </a>
+        <a class="item" href="../views/usuario_login.php">
+            Logar
+        </a>
+        <a class="item" href="../views/usuario_cadastro.php">
+            Cadastrar
         </a>
     </div>
-</div>
 </nav>
-<?php endif; ?>
+<?php else: ?>
+    <nav>
+        <div class="ui inverted menu">
+            <a class="active item" href="../views/home.php">
+                Home
+            </a>
+            <?php
+            if (isset($_SESSION['logado']) and $_SESSION['logado'] = 'sim') {
+                ?>
+                <div class="item right menu">
+                    <i class="user outline icon"></i>
+                </div>
+                <a href="../views/usuario_pagina.php">
+                    <div class="right item">
+                        <?php
+                        echo $_SESSION['primeiro_nome'];
+                        ?>
+                    </div>
+                </a>
+                <?php
+            }
+            if (isset($_SESSION['mercado']['logado']) and $_SESSION['mercado']['logado'] = 'sim') {
+                ?>
+                <a href="../views/mercado_paginaLog.php">
+                    <div class="right item">
+                        <?php
+                        echo $_SESSION['mercado']['nome_mercado']
+                        ?>
+                    </div>
+                </a>
+                <?php
+            }
+            ?>
+            <div class="right menu">
+                <a href="../controllers/usuario.php?acao=usuario_deslogar" class="ui item">
+                    Deslogar
+                </a>
+            </div>
+        </div>
+    </nav>
+    <?php endif; ?>
